4d088e346ac5b9a7f78177a735da3e6ebcdcf889
Samuel Carrington <samuelcarrington@gmail.com>
2016-01-14 21:09:41 -0500

    init commit

---
+++ ex_6_1_for_loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_6_1_for_loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* 6-1  Write a program that prints on the console the numbers from 1 to N. 
+            The number N should be read from the standard input. (Chapter 6, Exercise 1)
+            */
+
+            // Ask for input
+            Console.WriteLine("This program prints on the console the numbers from 1 to N");
+            Console.WriteLine("N=");
+            int N = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("Number : " + i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
